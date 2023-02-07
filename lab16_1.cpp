6867ad1fbd182d5d9f9c1183b30660e4e896cf29
napapon-wongkom <dot.wongkom@gmail.com>
2023-02-07 15:12:54 +0700

    lab16_1.cpp

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -1,9 +1,55 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
 
-	
+int main(){
+	int a = 5;
+	string b ="A";
+	string &c = b;
+	int *x = &a;
+	string *y = &b;
+	int *z = x;
+	cout<<a<<" ";
+	cout<<b<<" ";
+	cout<<c<<" ";
+	cout<<x<<" ";
+	cout<<y<<" ";
+	cout<<z<<"\n";
+
+	cout<<&a<<" ";
+	cout<<&b<<" ";
+	cout<<&c<<" ";
+	cout<<&x<<" ";
+	cout<<&y<<" ";
+	cout<<&z<<"\n";
+	c = "F";
+	cout<<a<<" ";
+	cout<<b<<" ";
+	cout<<c<<" ";
+	cout<<x<<" ";
+	cout<<y<<" ";
+	cout<<z<<"\n";
+	*y ="W";
+	cout<<a<<" ";
+	cout<<b<<" ";
+	cout<<c<<" ";
+	cout<<x<<" ";
+	cout<<y<<" ";
+	cout<<z<<"\n";
+	*x = 6;
+	cout<<a<<" ";
+	cout<<b<<" ";
+	cout<<c<<" ";
+	cout<<x<<" ";
+	cout<<y<<" ";
+	cout<<z<<"\n";
+	*z = 7;
+	cout<<a<<" ";
+	cout<<b<<" ";
+	cout<<c<<" ";
+	cout<<x<<" ";
+	cout<<y<<" ";
+	cout<<z<<"\n";
 	
 	return 0;
 }
